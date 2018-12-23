2e34b5867966c1ce8d57f9a8774b0188beb5bcc7
ViolaCreativa <violaprinz@web.de>
2018-12-23 18:51:13 +0100

    WriteJquery code.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log("Hello, I'am happy about your visit. Welcomw to my Portfolio");
+console.log("Hello, I'am happy about your visit. Welcome to my Portfolio");
+$(document).ready(function() {
+  $(".project-preview").on("click", function() {
+    $(this).siblings(".project-details").slideToggle();
+    });
+});
